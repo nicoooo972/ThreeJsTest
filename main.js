7a8125807f07070b3d8a8767cf96029fde1a5642
lebozec <nicolas.lebozec@xelians.fr>
2023-11-24 13:42:50 +0100

    Changement dans l'import de la librarie

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://threejs.org/build/three.module.js';
+import * as THREE from 'three';
 
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -58,3 +58,7 @@ import * as THREE from 'https://threejs.org/build/three.module.js';
     };
 
     animate();
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
